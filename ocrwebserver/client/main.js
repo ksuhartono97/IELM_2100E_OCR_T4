030cd929af77278c5b1e76f4b3da74e81084b206
Kristian Suhartono <blackxephos2@gmail.com>
2017-04-25 12:54:28 +0800

    Finished implementation of database search with OCR results, boosted with levenshtein similarity

--- ocrwebserver/client/main.js
+++ ocrwebserver/client/main.js
@@ -22,6 +22,8 @@ var fbDatabase = firebase.database().ref();
 var fbImages = fbDatabase.child('images');
 
 var currentContext = new ReactiveVar();
+var outputStrings = new ReactiveVar();
+var searchResult = new ReactiveVar();
 
 Tracker.autorun(function() {
     FlowRouter.watchPathChange();
@@ -112,6 +114,62 @@ Template.uploadcsv.events({
     }
 });
 
-Template.uploadcsv.onCreated( () => {
+Template.ocr.onCreated( () => {
+    Template.instance().uploading = new ReactiveVar( false );
     Meteor.subscribe("skudata.all");
+});
+
+Template.ocr.helpers({
+    uploading() {
+        return Template.instance().uploading.get();
+    },
+    outputString() {
+        return Session.get("outputStrings");
+    },
+    searchResult() {
+        return Session.get("searchResult");
+    }
+});
+
+Template.ocr.events({
+   'change [name="getocrdata"]' (event, template) {
+       template.uploading.set(true);
+
+       let tempReader = new FileReader();
+       tempReader.onload = function (e) {
+           //Update the DOM with the fetched string
+           Session.set('outputStrings', e.target.result);
+
+           //Split the string
+           let res = e.target.result.split(/\r?\n/);
+
+           //Construct an array and do matching with levenshtein with database
+           let simArray = [];
+           const skudata = SKUData.find().fetch();
+
+           skudata.forEach((data) => {
+              let simScore = 0;
+              simScore += levenshtein.get(res[0], data.UniqloAttrValue1);
+              simScore += levenshtein.get(res[1], data.UniqloAttrValue2);
+              simArray.push({id:data._id, sim: simScore});
+           });
+
+           //Find the best matched object
+           let minIndex = 0;
+           for(let i = 0; i < simArray.length; i++) {
+               if(simArray[i].sim < simArray[minIndex].sim) {
+                   minIndex = i;
+               }
+           }
+
+           //Do a retrieval from database of best object, update the html view
+           const relatedDatabaseResult = SKUData.find({_id:simArray[minIndex].id}).fetch();
+           Session.set('searchResult', relatedDatabaseResult);
+
+           //Complete the operation
+           template.uploading.set( false );
+           Bert.alert( 'Upload complete!', 'success', 'growl-top-right' );
+       };
+       tempReader.readAsText(event.target.files[0]);
+   }
 });
